cf00006bf3c441633164e7ad2c5977c5f172e49b
duke-doki <145125297+duke-doki@users.noreply.github.com>
2023-10-04 17:22:32 +0300

    Update
    readme, settings, env

--- README.md
+++ README.md
@@ -8,6 +8,8 @@ and other required information. The main purpose is to check if visitor suspicio
 ### How to install
 
 This project uses env variables such as website address and passwords. They can be setted up via `settings.py`.
+The main ones are `DB_DEBUG`, `DB_HOST`, `DB_PASSWORD`, `DB_SECRET_KEY`, `DB_ALLOWED_HOSTS`. They
+respectively affect debugging, database's address and password, secret key and also allowed local hosts.
 
 Python3 should be already installed. 
 Then use `pip` (or `pip3`, if there is a conflict with Python2) to install dependencies:
--- project/settings.py
+++ project/settings.py
@@ -6,24 +6,24 @@ env.read_env()
 
 DATABASES = {
     'default': {
-        'ENGINE': env('DB_ENGINE'),
-        'HOST': env('DB_HOST'),
-        'PORT': env('DB_PORT'),
-        'NAME': env('DB_NAME'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
+        'ENGINE': env.str('DB_ENGINE'),
+        'HOST': env.str('DB_HOST'),
+        'PORT': env.str('DB_PORT'),
+        'NAME': env.str('DB_NAME'),
+        'USER': env.str('DB_USER'),
+        'PASSWORD': env.str('DB_PASSWORD'),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = env('DB_SECRET_KEY')
+SECRET_KEY = env.str('DB_SECRET_KEY')
 
-DEBUG = env('DB_DEBUG').capitalize() == 'True'
+DEBUG = env.bool('DB_DEBUG', default=False)
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list('DB_ALLOWED_HOSTS')
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
